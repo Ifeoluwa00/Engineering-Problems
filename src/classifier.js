6b18996c49283d762c81a3d7c943e11893c6d679
olukayode ifeoluwa <ifeoluwajoshua2015@gmail.com>
2021-07-10 00:22:05 +0100

    Completed week 2 task

--- src/classifier.js
+++ src/classifier.js
@@ -4,6 +4,55 @@
  *
  * @param {any} input Array of student objects
  */
-function classifier(input) {}
+function classifier(input) {
+  if (input.length === 0) {
+    return {
+      noOfGroups: 0,
+    };
+  }
+
+  let personnel = input
+    .map((user) => {
+      user['age'] =
+        new Date(Date.now()).getFullYear() -
+        new Date(user['dob']).getFullYear();
+      return user;
+    })
+    .sort((a, b) => a.age - b.age);
+
+  let majorClass = [];
+  let minorClass = [];
+  for (let index = 0; index < personnel.length; index++) {
+    if (minorClass.length === 0) {
+      minorClass.push(personnel[index]);
+    } else if (personnel[index].age - minorClass[0].age <= 5) {
+      minorClass.push(personnel[index]);
+    } else if (personnel[index].age - minorClass[0].age > 5) {
+      majorClass.push(minorClass);
+      minorClass = [];
+      minorClass.push(personnel[index]);
+    }
+
+    if (minorClass.length === 3 || index + 1 >= personnel.length) {
+      majorClass.push(minorClass);
+      minorClass = [];
+    }
+  }
+
+  let compiler = majorClass.reduce((element, group, index) => {
+    element['noOfGroups'] = majorClass.length;
+    element[`group${index + 1}`] = {};
+    let minorGroup = element[`group${index + 1}`];
+    minorGroup['members'] = group;
+    let ages = group.map((user) => user.age);
+    minorGroup['oldest'] = ages[ages.length - 1];
+    minorGroup['sum'] = ages.reduce((total, age) => total + age);
+    let nos = group.map((user) => Number(user.regNo)).sort((a, b) => a - b);
+    minorGroup['regNos'] = nos;
+    return element;
+  }, {});
+
+  return compiler;
+}
 
 module.exports = classifier;
--- src/laundry.js
+++ src/laundry.js
@@ -4,6 +4,40 @@
  *
  * @returns {any} Trip data analysis
  */
-function getMaxPairs(noOfWashes, cleanPile, dirtyPile) {}
+function getMaxPairs(noOfWashes, cleanPile, dirtyPile) {
+  let cleanSocks = 0;
+  for (let index = 0; index < cleanPile.length; index++) {
+    if (
+      cleanPile.includes(cleanPile[index], index + 1) &&
+      cleanPile.length !== index + 1
+    ) {
+      let value = cleanPile.lastIndexOf(cleanPile[index]);
+      cleanPile.splice(value, 1);
+      cleanSocks += 1;
+    } else if (dirtyPile.includes(cleanPile[index])) {
+      if (noOfWashes > 0) {
+        let value = dirtyPile.indexOf(cleanPile[index]);
+        dirtyPile.splice(value, 1);
+        cleanSocks += 1;
+        noOfWashes -= 1;
+      }
+    }
+  }
+
+  for (let item = 0; item < dirtyPile.length; item++) {
+    if (noOfWashes > 1) {
+      if (
+        dirtyPile.includes(dirtyPile[item], item + 1) &&
+        dirtyPile.length !== item + 1
+      ) {
+        let value = dirtyPile.lastIndexOf(dirtyPile[item]);
+        dirtyPile.splice(value, 1);
+        cleanSocks += 1;
+        noOfWashes -= 2;
+      }
+    }
+  }
+  return cleanSocks;
+}
 
 module.exports = getMaxPairs;
